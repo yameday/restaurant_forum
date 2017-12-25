9a5bfcb5eabff6b5cf660f1057a722e57e229f54
sakuraki <a1989198@hotmail.com>
2017-12-25 23:54:07 +0800

    INIT

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,57 @@
+class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+  
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+  
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  before_action :set_restaurant, only:  [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurant_path(@restaurant)
+      flash[:notice] = "restaurant was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "restaurant was failed to update"
+    end
+  end
+  
+  def destroy
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "restaurant was deleted"
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
+  end
+  
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "restaurants#index"
+
+  namespace :admin do
+    resources :restaurants
+    root "restaurants#index"
+  end
+
+end
