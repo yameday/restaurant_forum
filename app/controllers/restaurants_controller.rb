3914964077b905e045a3e8f31b0ccf1af86af55e
sakuraki <a1989198@hotmail.com>
2018-01-06 14:14:58 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,8 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
   end
+  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
