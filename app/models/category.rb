176ac4f6992a8d4de0a75aae67c832e893c92653
sakuraki <a1989198@hotmail.com>
2018-01-07 00:17:50 +0800

    add dependency between models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates_presence_of :name
 
-  has_many :restaurants, dependent: :destroy
+   # 如果分類下已有餐廳，就不允許刪除分類（刪除時拋出 Error）
+  has_many :restaurants, dependent: :restrict_with_error
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
 
-  belongs_to :category, optional: true  
-  has_many :comments
+  belongs_to :category
+  # 當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
+  has_many :comments, dependent: :destroy
 end
