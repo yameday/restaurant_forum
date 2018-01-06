a011a806a98141345c515a57b22ec864898093c9
sakuraki <a1989198@hotmail.com>
2018-01-06 14:39:39 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class Admin::RestaurantsController < ApplicationController
-  before_action :authenticate_user!
+  
   before_action :authenticate_admin
   
   def index
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :authenticate_user!
+ 
   
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
