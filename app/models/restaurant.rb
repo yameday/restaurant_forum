ece812e50a7323f84a3ae19af3b93aa655c6e69a
sakuraki <a1989198@hotmail.com>
2018-01-25 00:56:05 +0800

    setup relation among user, restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,7 @@ class Restaurant < ApplicationRecord
   belongs_to :category
   # 當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
   has_many :comments, dependent: :destroy
+  
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
