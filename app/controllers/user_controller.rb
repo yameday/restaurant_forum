1ce0ee48ed9ade6ecc4612bd8a54cbadf8c5f6b1
sakuraki <a1989198@hotmail.com>
2018-01-30 23:53:44 +0800

    user can upload avatar

--- app/controllers/user_controller.rb
+++
@@ -1,6 +0,0 @@
-class UserController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @commented_restaurants = @user.restaurants
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   
   validates_presence_of :name
   
+  mount_uploader :avatar, AvatarUploader
+  
   # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
